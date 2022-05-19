7f5b5670359268ba7fbad250ec5ccb673d7d6c3e
g-oliveira7 <studying.online@outlook.com>
2022-05-19 10:08:42 -0300

    Projeto inicial

---
+++ backend/src/test/java/com/brgabrieldeoliveira/springrestalgaworks/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brgabrieldeoliveira.springrestalgaworks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
