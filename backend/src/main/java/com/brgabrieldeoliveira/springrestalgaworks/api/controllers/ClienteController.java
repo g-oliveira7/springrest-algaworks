e717533676eca226ba0a8b922ad6af6d02cf090e
g-oliveira7 <studying.online@outlook.com>
2022-05-19 16:25:07 -0300

    Serviços CRUD para Clientes

--- backend/src/main/java/com/brgabrieldeoliveira/springrestalgaworks/api/controllers/ClienteController.java
+++ backend/src/main/java/com/brgabrieldeoliveira/springrestalgaworks/api/controllers/ClienteController.java
@@ -3,7 +3,14 @@ package com.brgabrieldeoliveira.springrestalgaworks.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +26,31 @@ public class ClienteController {
 
 	@GetMapping
 	public List<Cliente> listarTodos() {
-		List<Cliente> lista = clienteService.listarTodos();
-		return lista;
+		return clienteService.listarTodos();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+		return clienteService.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente) {
+		return clienteService.salvar(cliente).map(c -> ResponseEntity.status(HttpStatus.CREATED).body(cliente))
+				.orElse(ResponseEntity.badRequest().build());
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente dadosParaAtualizar) {
+		return clienteService.atualizar(id, dadosParaAtualizar).map(c -> ResponseEntity.ok(c))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletar(@PathVariable Long id) {
+		return clienteService.buscarPorId(id).map(c -> {
+			clienteService.deletarPorId(c.getId());
+			return ResponseEntity.noContent().build();
+		}).orElse(ResponseEntity.notFound().build());
 	}
 }
--- backend/src/main/java/com/brgabrieldeoliveira/springrestalgaworks/api/services/ClienteService.java
+++ backend/src/main/java/com/brgabrieldeoliveira/springrestalgaworks/api/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.brgabrieldeoliveira.springrestalgaworks.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,51 @@ public class ClienteService {
 	public List<Cliente> listarTodos() {
 		List<Cliente> lista = clienteRepository.findAll();
 		return lista;
+	}
+
+	public Optional<Cliente> buscarPorId(Long id) {
+		if (id == null) {
+			return null;
+		}
+		
+		return clienteRepository.findById(id);
+	}
+
+	public Optional<Cliente> salvar(Cliente cliente) {
+		if (cliente == null) {
+			return null;
+		}
+		
+		return Optional.of(clienteRepository.save(cliente));
+	}
+
+	public Optional<Cliente> atualizar(Long id, Cliente dadosParaAtualizar) {
+		Cliente clienteParaAtualizar = buscarPorId(id).orElse(null); 
+		
+		if (clienteParaAtualizar != null) {			
+			return Optional.of(atualizarCliente(clienteParaAtualizar, dadosParaAtualizar));
+		}
+		return null;
+	}
+
+	private Cliente atualizarCliente(Cliente clienteParaAtualizar, Cliente dadosParaAtualizar) {
+		final String nome = dadosParaAtualizar.getNome();
+		final String email = dadosParaAtualizar.getEmail();
+		final String telefone = dadosParaAtualizar.getTelefone();
+		
+		if (nome != null) {
+			clienteParaAtualizar.setNome(nome);
+		}
+		if (email != null) {
+			clienteParaAtualizar.setEmail(email);
+		}
+		if (telefone != null) {
+			clienteParaAtualizar.setTelefone(telefone);
+		}
+		return clienteParaAtualizar;
+	}
+
+	public void deletarPorId(Long id) {
+		clienteRepository.deleteById(id);		
 	}
 }
